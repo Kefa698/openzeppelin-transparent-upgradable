9dc6fbfb748830ec365061cb43421504e9a208dd
Kefa698 <kefaombongi07@gmail.com>
2022-12-18 13:02:26 +0300

    deploy

---
+++ script/deploy.js
@@ -0,0 +1,14 @@
+const { ethers, upgrades } = require("hardhat")
+
+async function main() {
+    const Box = await ethers.getContractFactory("Box")
+    const box = await upgrades.deployProxy(Box, [42], { initializer: "initialize" })
+    await box.deployed()
+    console.log("Box deployed to:", box.address)
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
